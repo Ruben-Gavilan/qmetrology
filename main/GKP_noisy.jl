351d81c366a3dd7de5a13f4177bf5f58266b3f95
Ruben-Gavilan <ruben.gavilan.olaya@gmail.com>
2026-06-30 11:48:12 +0200

    Update and rename gekape_ruidoso.jl to GKP_noisy.jl

--- main/GKP_noisy.jl
+++ main/GKP_noisy.jl
@@ -1,23 +1,22 @@
-# Script específico para GKP, ajustando el número de fotones para que encaje con el resto.
-
+# Specific script for GKP state for noisy environment
 include("../modulos/fisher_functions.jl")
 using PyPlot,Polynomials,Printf
 
-N=75 # Para un maximo de 2.76 está convergido a partir de ~30
+N=60
 phi=2.0
 dl=1e-4
-tipo="GKP2"
+tipo="GKP"
 
 T=1e-4;
-kappa=300; #Valor típìco en microondas (REVISAR)
+kappa=300;
 time=LinRange(0.0,T,2);
 
 n_GKP=[1.84,2.11,2.33,2.55,2.76]
 niter=length(n_GKP)
 
-numero= tensor(num(N),eye(N)) + tensor(eye(N),num(N))
+number= tensor(num(N),eye(N)) + tensor(eye(N),num(N))
 QFI=zeros(niter)
-fotones=zeros(niter)
+photons=zeros(niter)
 
 c_ops=[sqrt(kappa)*tensor(destroy(N),eye(N)), sqrt(kappa)*tensor(eye(N),destroy(N))]
 
@@ -37,31 +36,17 @@ for i=1:niter
 	rho0=mesolve(zero(tensor(eye(N),eye(N))),psi0,time,c_ops, e_ops=[ ],progress_bar=Val(false))
 	rho0=rho0.states[2];
 	
-	fotones[i]=abs(tr(rho0*numero))
+	photons[i]=abs(tr(rho0*number))
 	QFI[i]=get_QFI_derivada(rho0,drho)
 	drho=nothing
 	rho0=nothing
 	@printf("Iteración número: %i\n",i)
 end
 
-io=open("../../resultados/final/"*tipo*"_QFI_300_final.txt","w")
+io=open(tipo*"_QFI_300_final.txt","w")
 @printf(io,"#N=%i;	n=%i;	dl=%1.1e\n",N,niter,dl)
 @printf(io,"fot, QFI\n")
 for i=1:length(QFI)
-	@printf(io,"%9.5f %9.5f \n",fotones[i],QFI[i])
+	@printf(io,"%9.5f %9.5f \n",photons[i],QFI[i])
 end
 close(io)
-
-#reg1=fit(log.(fotones),log.(1 ./QFI),1)
-#label1=@sprintf("QFI, m=%f",reg1[1])
-#plot(fotones,1 ./QFI,"sb",label=label1)
-#plot(fotones,1 ./(fotones.^2),"^r",label="HL")
-#plot(fotones, 1 ./fotones,"^g",label="SQL")
-#xscale("log")
-#yscale("log")
-#legend()
-#grid()
-#titulito=@sprintf("GKP State, N=%i, dl=%1.1e",N,dl)
-#title(titulito)
-#xlabel("Número de fotones")
-#ylabel("Delta phi^2")
